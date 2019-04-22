c74346cc2f782451a202f55b79de2a9359166e06
schmitzaddie <schmitzaddie@gmail.com>
2019-04-22 16:24:17 -0500

    Adding files

---
+++ app/data/friends.js
@@ -0,0 +1,94 @@
+var friends = [
+    {
+        name: "Rachel",
+        photo: "http://images5.fanpop.com/image/photos/29900000/Rachel-rachel-green-29922108-200-200.jpg",
+        scores: [
+            "2",
+            "2",
+            "5",
+            "2",
+            "2",
+            "3",
+            "1",
+            "1",
+            "3"
+        ]
+    },
+    {
+        name: "Ross",
+        photo: "https://vignette.wikia.nocookie.net/friends/images/8/89/Square_Ross.jpg/revision/latest/scale-to-width-down/200?cb=20111216200027",
+        scores: [
+            "1",
+            "3",
+            "4",
+            "4",
+            "5",
+            "6",
+            "2",
+            "4",
+            "6"
+        ]
+    },
+    {
+        name: "Phoebe",
+        photo: "https://i.pinimg.com/236x/5b/ef/82/5bef82986de31e70eb2dafcbfa9ccdd5--phoebe-buffay-icons.jpg",
+        scores: [
+            "3",
+            "1",
+            "1",
+            "1",
+            "1",
+            "2",
+            "1",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "Joey",
+        photo: "http://images4.fanpop.com/image/photos/22500000/Joey-friends-22529019-200-200.jpg",
+        scores: [
+            "5",
+            "5",
+            "3",
+            "3",
+            "4",
+            "4",
+            "5",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: "Monica",
+        photo: "http://images4.fanpop.com/image/photos/22500000/Monica-friends-22554243-200-200.jpg",
+        scores: [
+            "1",
+            "4",
+            "2",
+            "5",
+            "6",
+            "5",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Chandler",
+        photo: "https://gl-images.condecdn.net/image/yvPZrZ5AYpY/crop/200/square/f/S1_Chandler_3f874dc7_P.jpg",
+        scores: [
+            "4",
+            "1",
+            "5",
+            "6",
+            "3",
+            "1",
+            "4",
+            "3",
+            "4"
+        ]
+    }
+];
+
+module.exports = friends;
